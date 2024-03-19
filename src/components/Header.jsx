8e39530ec4cef898e9f3f88f681e53c62225170a
samuelleitee <programmer.and.investor@gmail.com>
2024-03-19 20:37:57 -0300

    feat: changed Header to a separated file

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export function Header() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="produtos">Produtos</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
